e1e770d3a1c2b856b932688242626319fd1e9638
jlmayo <jlmayo22@gmail.com>
2022-11-27 20:57:20 -0700

    Made js html qs cnxns. Search history column.

---
+++ assets/js/script.js
@@ -0,0 +1,40 @@
+var cities = [];
+
+var citySearchForm=document.querySelector("#city-search-form");
+var cityInput=document.querySelector("#city");
+var currentWeather=document.querySelector("#current-weather-container");
+var currentCity=document.querySelector("#current-city");
+var fiveDayForecastTitle=document.querySelector("#forecast");
+var fiveDayForecast=document.querySelector("#fiveday-container");
+var searchHistoryButtons=document.querySelector("#search-history-buttons");
+
+
+var formSubmitHandler = function(event){
+    event.preventDefault();
+    var city = cityInput.ariaValueMax.trim();
+    if(city){
+        cityWeather(city);
+        fiveDayForecast(city);
+        cities.unshift({city});
+        cityInput.value = "";
+    } else {
+        alert("Please enter a valid city.");
+    }
+    saveSearchHistory();
+    searchHistory(city);
+}
+
+var saveSearchHistory = function() {
+    localStorage.setItem("cities", JSON.stringify(cities));
+};
+
+var searchHistory = function(searchHistory){
+
+    searchHistoryList = document.createElement("button");
+    searchHistoryList.textContent = searchHistory;
+    searchHistoryList.classList = "btn btn-dar btn-lg btn-block p-2";
+    searchHistoryList.setAttribute("data-city", searchHistory);
+    searchHistoryList.setAttribute("type", "submit");
+
+    searchHistoryButtons.prepend(searchHistory);
+}
